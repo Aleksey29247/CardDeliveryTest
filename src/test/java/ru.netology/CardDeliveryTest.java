044fc7868beb0e320575bd111b0a3fcfe255cfbb
Aleksey <Alik29247@gmail.com>
2022-04-02 19:46:39 +0700

    initial commit

--- src/test/java/ru.netology/CardDeliveryTest.java
+++ src/test/java/ru.netology/CardDeliveryTest.java
@@ -11,10 +11,10 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-
 
 
 
@@ -24,7 +24,7 @@ public class CardDeliveryTest {
         Configuration.browserSize = "800x800";
         open("http://localhost:9999/");
         $$x("//input[@type= 'text']").get(0).val("Новосибирск");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $(" [data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $$x("//input[@type='tel']").get(0).val("05.04.2022");
         $$x("//input[@type='text']").get(1).val("Пригода Елена Дмитриевна");
         $$x("//input[@type='tel']").get(1).val("+79237481592");
@@ -36,6 +36,7 @@ public class CardDeliveryTest {
 
 
     }
+
 
 
 
