70fbc15d10b760a8d35555038ab8cf132fd89ba1
Aleksey <Alik29247@gmail.com>
2022-04-03 16:18:29 +0700

    initial commit

--- src/test/java/ru.netology/CardDeliveryTest.java
+++ src/test/java/ru.netology/CardDeliveryTest.java
@@ -17,25 +17,26 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardDeliveryTest {
 
 
-
-    @Test
-    public void souldSendForm() {
-        Configuration.holdBrowserOpen = true;
+   @Test
+   public void souldSendForm() {
+       Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "800x800";
-        open("http://localhost:9999/");
-        $$x("//input[@type= 'text']").get(0).val("Новосибирск");
-        $(" [data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $$x("//input[@type='tel']").get(0).val("05.04.2022");
+       open("http://localhost:9999/");
+       $$x("//input[@type= 'text']").get(0).val("Новосибирск");
+       $(" [data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+       $$x("//input[@type='tel']").get(0).val("09.04.2022");
         $$x("//input[@type='text']").get(1).val("Пригода Елена Дмитриевна");
-        $$x("//input[@type='tel']").get(1).val("+79237481592");
-        $("[data-test-id='agreement']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(10));
-
-            }
-
+       $$x("//input[@type='tel']").get(1).val("+79237481592");
+       $("[data-test-id='agreement']").click();
+       $$("button").find(exactText("Забронировать")).click();
+       $$("button").find(exactText("Успешно!"));
 
     }
+
+
+
+
+}    
 
 
 
