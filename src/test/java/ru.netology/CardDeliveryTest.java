e780215495a6ca5bf99dba816e1c07667d945c84
Aleksey <Alik29247@gmail.com>
2022-04-08 21:36:11 +0700

    initial commit

--- src/test/java/ru.netology/CardDeliveryTest.java
+++ src/test/java/ru.netology/CardDeliveryTest.java
@@ -1,35 +1,43 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
 
-   @Test
-   public void souldSendForm() {
-       Configuration.holdBrowserOpen = true;
+    @Test
+    public void souldSendForm() {
+        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "800x800";
-       open("http://localhost:9999/");
-       $$x("//input[@type= 'text']").get(0).val("Новосибирск");
-       $(" [data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-       $$x("//input[@type='tel']").get(0).val("09.04.2022");
-        $$x("//input[@type='text']").get(1).val("Пригода Елена Дмитриевна");
-       $$x("//input[@type='tel']").get(1).val("+79237481592");
-       $("[data-test-id='agreement']").click();
-       $$("button").find(exactText("Забронировать")).click();
-       $$("button").find(exactText("Успешно!"));
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").val("Новосибирск");
+        $(" [data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@type= 'tel']").val(planningDate);
+        $("[data-test-id=name] input").setValue("Пригода Елена Дмитриевна");
+        $("[data-test-id=phone] input").setValue("+79237481592");
+        $("[data-test-id='agreement']").click();
+        $$(withText("Забронировать")).first().click();
+        $("[class='notification__content']")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + (planningDate)), Duration.ofSeconds(15));
+    }
+
+
+    String planningDate = generateDate(4);
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
     }
 
     @Test
@@ -37,9 +45,17 @@ public class CardDeliveryTest {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "800x800";
         open("http://localhost:9999/");
-        $$x("//input[@type= 'text']").get(0).val("Но");
-        $(withText("Новосибирск")).click();
-         }
+        $("[data-test-id='city'] input").val("Бердск");
+        $(" [data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@type= 'tel']").val(planningDate);
+        $("[data-test-id=name] input").setValue("Пригода Елена Дмитриевна");
+        $("[data-test-id=phone] input").setValue("+79237481592");
+        $("[data-test-id='agreement']").click();
+        $$(withText("Забронировать")).first().click();
+        $("[data-test-id = 'city'] .input__sub")
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
 }    
 
 
